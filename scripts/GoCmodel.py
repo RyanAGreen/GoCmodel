7fcb951b2ded04be72213ff2a420b79a8a688e0e
Ryan Green <rygreen@ucsc.edu>
2022-04-12 12:36:05 -0700

    removed functions that are not yet implemented in GoC model

--- scripts/GoCmodel.py
+++ scripts/GoCmodel.py
@@ -83,13 +83,6 @@ class GoCModel:
         self.output = None
         self.tracers_arr = np.zeros((5, 5, 1))
 
-        # yet to be put into the model
-        # initialize biological pump export
-        self.num_surf = 1
-        self.num_interior = 2
-        # Export matrix; fraction of export from surface (column) to interior (row)
-        self.export_matrix = np.array([[1, 0, 0], [0, 0, -1], [0, 1, 0]])
-
     def make_state_a(self, state_v, time):
         """Gets called every year and makes new state in matrix format. Boxes are in columns and tracers are in
         rows.
@@ -145,44 +138,6 @@ class GoCModel:
         d_dt[4, 1] = -1000 * carbon_flux_goc_subsurface
 
         return d_dt
-
-    def production(self, state_a):
-        """this could be used to calculate bio pump"""
-        net_primary_prod = (
-            4 * state_a[0, 0:5] * self.mass[0:5]
-        )  # 1/yr * µmol/kg * kg = µmol/yr
-        d_dt = np.zeros((self.num_tracer, self.num_box))
-        d_dt[0, :] = self.export_matrix @ net_primary_prod
-        d_dt[1, :] = self.export_matrix @ net_primary_prod / 16
-        d_dt[2, :] = self.export_matrix @ (
-            net_primary_prod * (state_a[2, 0:5] / state_a[0, 0:5] - self.epsi_assim)
-        )
-        return (
-            d_dt,
-            net_primary_prod * 1e-6 * 1e-12 * 14,
-            (state_a[2, 0:5] / state_a[0, 0:5] - self.epsi_assim),
-        )
-
-    def export_phosphorus(self, state):
-        """computes phosphorus export"""
-        export_phos = np.zeros(3).T
-        state = state[:, :-2]
-        phos = state.reshape(3, 6)[:, 2] / self.mass[:]  # mol/kg P
-        set_phos = np.array([1e-6, 1e-7])
-        for surf_boxes in range(0, self.num_surf):
-            timescale = 20  # year
-            diff = phos[surf_boxes] - set_phos[surf_boxes]
-            if diff > 0:
-                export_phos[surf_boxes] = (
-                    diff / timescale * self.mass[surf_boxes]
-                )  # mol surfacePO4/year
-
-            else:
-                # print(P[s],SetP[s],P[s]-SetP[s])
-                pass  # not enough nutrients to sustain productivity
-        print(self.export_matrix)
-        print(export_phos)
-        return self.export_matrix @ export_phos
 
     def carb_chem(self):
         """
