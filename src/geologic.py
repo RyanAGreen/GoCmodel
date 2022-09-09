e686c4d5214f80043324818fa8345b7bed70f656
Ryan Green <rygreen@ucsc.edu>
2022-09-09 08:36:26 -0700

    Moved manual geologic carbon add to a module

---
+++ src/geologic.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+
+def manual_carbon_add(num_tracer, num_box, rate, box_name, mass_array):
+    """geologic carbon addition"""
+
+    if box_name == "marchitto":
+        i = 0
+    elif box_name == "subsurface":
+        i = 1
+    elif box_name == "surface":
+        i = 2
+
+    carbon_flux = rate * 1e15 / 12 * 1e6 / mass_array[i]
+
+    d_dt = np.zeros((num_tracer, num_box))
+    # DIC
+    d_dt[0, i] = carbon_flux
+    # ALK
+    d_dt[1, i] = 1 * carbon_flux
+    # d13C
+    d_dt[3, i] = -9 * carbon_flux
+    # D14C
+    d_dt[4, i] = -1000 * carbon_flux
+    # Cum Carbon
+    d_dt[5, i] = rate
+
+    return d_dt
+
