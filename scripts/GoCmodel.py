adf86e8edd9f2caee4a5ba0457e7495bdab82a53
Ryan Green <rygreen@ucsc.edu>
2022-09-09 08:46:06 -0700

    put carbonate chemistry into its own module

--- scripts/GoCmodel.py
+++ scripts/GoCmodel.py
@@ -204,27 +204,6 @@ class GoCModel:
 
         return d_dt
 
-    def carb_chem(self, surface_alk, surface_dic):
-        """
-        Converts DIC and ALK to microles/kg then
-        uses pyCO2sys to solve carbonate chemistry
-        returns DIC speciation, pH, omega and pCO2
-        """
-        # dic = []
-        # alk = []
-        # for i in range(3):
-        #     dic.append(self.result.y[i, :])
-        #     alk.append(self.result.y[i + 3, :])
-
-        carbon_chemistry = pyco2.sys(
-            par1=surface_alk, par2=surface_dic, par1_type=1, par2_type=2
-        )
-        values = ["HCO3", "CO3", "CO2", "pH", "saturation_calcite", "pCO2", "k_CO2"]
-        carbonate_results = []
-        for term in values:
-            carbonate_results.append(carbon_chemistry[term])
-        return np.array(carbonate_results)
-
     def box_model(self, time, statev):
         # pylint: disable=unused-argument
         """
