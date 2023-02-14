cd4682a241b1de9ce24fd36c0df45634f30a1400
csun365 <chrisun1030@gmail.com>
2023-02-14 10:07:57 -0800

    added rate file

--- src/inputoutput.py
+++ src/inputoutput.py
@@ -56,11 +56,11 @@ def save_file(time, tracers, carbonate_chemistry):
         r"results/last_simulation_tracers.txt", df.values, fmt="%.2f", delimiter="\t"
     )
 
-def save_rates_GoC_file(time, tracers):
+def save_rates_GoC_file(time, tracers, carbonate_chemistry):
     df = save_file(time, tracers, carbonate_chemistry)
     rates = np.sum(df[:,-3:], axis=1)
     df_new = pd.DataFrame({"time": time/1000, "rate": rates})
-    np.savetxt(r"results/rates_GoC.txt", df_new.values, fmt="%.2f", delimiter="\t")
+    np.savetxt(r"results/R_GoC.txt", df_new.values, fmt="%.2f", delimiter="\t")
     return df_new.values
 
 def make_plot(time, tracers, carbonate_chemistry, mass):
